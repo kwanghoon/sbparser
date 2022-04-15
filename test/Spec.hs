f00c1177e1efe25b77c09dccb8167f6306e0ed9e
Kwanghoon Choi <lazyswamp@gmail.com>
2022-04-15 16:00:47 +0900

    bug fix

--- test/Spec.hs
+++ test/Spec.hs
@@ -29,12 +29,10 @@ spec = hspec $ do
             }
     
     it ("[Benchmark1] " ++ benchmark1_sb) $
-      do results <- computeCand False benchmark1_sb "" config_simple
-         mapM_ (item benchmark1_sb config) [1..9]  -- Max GS level (9) for Smallbasic
+      do mapM_ (item benchmark1_sb config) [1..9]  -- Max GS level (9) for Smallbasic
 
     it ("[Benchmark2] " ++ benchmark2_sb) $
-      do results <- computeCand False benchmark2_sb "" config_simple
-         mapM_ (item benchmark2_sb config) [1..9]  -- Max GS level (9) for Smallbasic
+      do mapM_ (item benchmark2_sb config) [1..9]  -- Max GS level (9) for Smallbasic
 
 item benchmark init_config gslevel = 
       do let test_config = init_config{config_GS_LEVEL=gslevel}
